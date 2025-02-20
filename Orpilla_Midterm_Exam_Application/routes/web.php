44d1d68f86f8c2170e72fedfbbca7de2bff89488
Jubibani <strawberryloli3@gmail.com>
2025-02-20 20:42:38 +0800

    wip: add route

--- Orpilla_Midterm_Exam_Application/routes/web.php
+++ Orpilla_Midterm_Exam_Application/routes/web.php
@@ -2,6 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/products', [App\Http\Controllers\ProductController::class, 'index']);
